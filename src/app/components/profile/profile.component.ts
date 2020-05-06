3b806952d17a3b0f0636784b23ff9c61d3592ce0
Arisa Rangklang <56140694+arifirst@users.noreply.github.com>
2020-05-06 14:14:37 +0700

    test up git lab2

---
+++ src/app/components/profile/profile.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+  id: string;
+  name: string;
+  year: number;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.id = "B6000035";
+    this.name = "Arisa Rangklang";
+    this.year = 1999;
+  }
+
+  getName(){
+    return this.name;
+  }
+
+}
